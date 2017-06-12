ca059c731020b23178b6d58ec985fd951e91c5f8
Gabe <gabriel.o.k@hotmail.com>
2017-06-11 22:42:43 -0700

    Stuck 2.1

--- app/src/main/java/com/example/gabriel/popularmovies/MovieFragment.java
+++ app/src/main/java/com/example/gabriel/popularmovies/MovieFragment.java
@@ -48,10 +48,12 @@ import static android.R.attr.fragment;
 
 public class MovieFragment extends Fragment {
     public static ImageAdapter mImageAdapter;
+    public static CursorAdapter mCursorAdapter;
 
     public MovieFragment() {
 
     }
+
     static ArrayList<MovieItem> movieList;
 
     @Override
@@ -63,11 +65,11 @@ public class MovieFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.movie_list, container, false);
-            movieList = new ArrayList<>();
-            ListView movieListView = (ListView) rootView.findViewById(R.id.list_listView);
-            mImageAdapter = new ImageAdapter(getActivity(), movieList);
-            movieListView.setAdapter(mImageAdapter);
+        View rootView = inflater.inflate(R.layout.movie_list, container, false);
+        movieList = new ArrayList<>();
+        ListView movieListView = (ListView) rootView.findViewById(R.id.list_listView);
+        mImageAdapter = new ImageAdapter(getActivity(), movieList);
+        movieListView.setAdapter(mImageAdapter);
 
 
         movieListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -82,28 +84,29 @@ public class MovieFragment extends Fragment {
                 movieArray[4] = mImageAdapter.getItem(position).getMovieVote();
                 movieArray[5] = mImageAdapter.getItem(position).getMovieTitle();
                 Intent detailIntent = new Intent(getActivity(), DetailActivity.class)
-                        .putExtra(Intent.EXTRA_TEXT,movieArray);
+                        .putExtra(Intent.EXTRA_TEXT, movieArray);
                 startActivity(detailIntent);
             }
         });
         return rootView;
     }
+
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_sort_type, menu);
 
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        int id =item.getItemId();
-        if (id == R.id.most_popular){
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.most_popular) {
             updateMovies();
         }
-        if (id==R.id.most_rated){
+        if (id == R.id.most_rated) {
             updateMovies2();
         }
-        if (id==R.id.favorites){
+        if (id == R.id.favorites) {
             FetchFavoriteMovie fetchFavoriteMovie = new FetchFavoriteMovie();
             FragmentManager fragmentManager = getFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -112,21 +115,25 @@ public class MovieFragment extends Fragment {
         }
         return true;
     }
+
     public void updateMovies2() {
         String hrated = "top_rated";
         MovieDBSyncAdapter.syncImmediately(getActivity(), hrated);
     }
+
     public void updateMovies() {
         String popular = "most_popular";
         MovieDBSyncAdapter.syncImmediately(getActivity(), popular);
     }
+
     @Override
-    public void onStart(){
+    public void onStart() {
         super.onStart();
-        if(isOnline(getActivity())){
-        updateMovies();
+        if (isOnline(getActivity())) {
+            updateMovies();
         }
     }
+
     public boolean isOnline(Context context) {
         ConnectivityManager cm =
                 (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -135,117 +142,5 @@ public class MovieFragment extends Fragment {
                 activeNetwork.isConnectedOrConnecting();
         return isConnected;
     }
-    public static class FetchMovieTask extends AsyncTask<String, Void, ArrayList<MovieItem>> {
-        private final String LOG_TAG = FetchMovieTask.class.getSimpleName();
-        private ArrayList<MovieItem> getMovieDataFromJson(String movieJsonStr)
-                throws JSONException{
-            final String MDB_results = "results";
-            final String MDB_Poster = "poster_path";
-            final String MDB_ID = "id";
-            final String MDB_synopsis = "overview";
-            final String MDB_date = "release_date";
-            final String MDB_title = "original_title";
-            final String MDB_vote = "vote_average";
-            movieList = new ArrayList<>();
-            JSONObject movieJson = new JSONObject(movieJsonStr);
-            JSONArray movieArray = movieJson.getJSONArray(MDB_results);
-
-            for(int i=0; i< movieArray.length(); i++){
-                String poster;
-                String id;
-                String synopsis;
-                String date;
-                String title;
-                String vote;
-                JSONObject popularResults = movieArray.getJSONObject(i);
-                poster = popularResults.getString(MDB_Poster);
-                id = popularResults.getString(MDB_ID);
-                synopsis = popularResults.getString(MDB_synopsis);
-                date = popularResults.getString(MDB_date);
-                title = popularResults.getString(MDB_title);
-                vote = popularResults.getString(MDB_vote);
-                MovieItem movieItem = new MovieItem(id, poster, synopsis, date, title, vote);
-                movieList.add(movieItem);
-            }
-            return movieList;
-
-        }
-
-
-
-
-
-        @Override
-        protected ArrayList<MovieItem> doInBackground(String... params) {
-            if (params.length == 0) {
-                return null;
-            }
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-            String movieJsonStr = null;
-            Bitmap PosterIcon;
-            try{
-                final String MDB_BASE_URL = "https://api.themoviedb.org/3/movie";
-                final String MDB_API_PARAMS = "api_key";
-                final String MDB_API_KEY = "8e445f0117d2e19e134382f9a2baf528";
-                Uri builtUri = Uri.parse(MDB_BASE_URL).buildUpon()
-                        .appendPath( params[0])
-                        .appendQueryParameter(MDB_API_PARAMS,MDB_API_KEY)
-                        .build();
-                URL url = new URL(builtUri.toString());
-                Log.v(LOG_TAG, "Built URI " + builtUri.toString());
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null){
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-                while ((line = reader.readLine()) !=null){
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    return  null;
-                }
-                movieJsonStr = buffer.toString();
-
-            }  catch (IOException e) {
-                e.printStackTrace();
-            }finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try{
-                        reader.close();
-                    } catch (IOException e) {
-                        Log.e(LOG_TAG,"Error closing stream", e);
-                    }
-                }
-            }
-            try{
-                return getMovieDataFromJson(movieJsonStr);
-            }catch (JSONException e){
-                Log.e(LOG_TAG, e.getMessage(), e);
-                e.printStackTrace();
-            }
-            return null;
-
-        }
-        //@Override
-        protected void onPostExecute(ArrayList<MovieItem> movies){
-            mImageAdapter.clear();
-            if (movies !=null){
-                try{
-            mImageAdapter.addAll(movies);
-            }catch (NullPointerException e) {
-                Log.e(LOG_TAG,"Error",e);}
-            }
-        }
-    }
 }
+
